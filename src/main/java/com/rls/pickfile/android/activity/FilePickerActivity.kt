fb9f5ed3523076e76c03b18fd36dc95cb320a908
xjd <771611622@qq.com>
2020-12-12 10:13:22 +0800

    add title bar

--- src/main/java/com/rls/pickfile/android/activity/FilePickerActivity.kt
+++ src/main/java/com/rls/pickfile/android/activity/FilePickerActivity.kt
@@ -2,15 +2,30 @@ package com.rls.pickfile.android.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.rls.pickfile.android.R
+import com.rls.pickfile.android.databinding.ActivityFilePickerBinding
+import com.rls.pickfile.android.viewmodel.FilePickerViewModel
 
 /**
  * Created by Ryan on 2020/12/12.
  */
 class FilePickerActivity : AppCompatActivity() {
 
+    private var mBinding: ActivityFilePickerBinding? = null
+    private var mViewModel: FilePickerViewModel? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_file_picker)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_file_picker)
+        mViewModel = ViewModelProvider(this).get(FilePickerViewModel::class.java)
+        mBinding?.viewModel = mViewModel
+        mBinding?.lifecycleOwner = this
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBinding = null
     }
 }
---
+++ src/main/java/com/rls/pickfile/android/viewmodel/FilePickerViewModel.kt
@@ -0,0 +1,11 @@
+package com.rls.pickfile.android.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * Created by Ryan on 2020/12/12.
+ */
+class FilePickerViewModel : ViewModel() {
+
+    var title: String? = "File Picker"
+}
